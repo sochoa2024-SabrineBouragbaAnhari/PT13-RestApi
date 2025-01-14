8437e9cfa1e73b5039c7db5a760d8af2a4bb49d1
SabrineBA <sabrinaanhari21@gmail.com>
2025-01-14 19:03:00 +0100

    Commit inicial

---
+++ app/src/main/java/net/iesseveroochoa/sabrinebouragba/t13_restapi/MainActivity.kt
@@ -0,0 +1,47 @@
+package net.iesseveroochoa.sabrinebouragba.t13_restapi
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import net.iesseveroochoa.sabrinebouragba.t13_restapi.ui.theme.T13RestApiTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            T13RestApiTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    T13RestApiTheme {
+        Greeting("Android")
+    }
+}
