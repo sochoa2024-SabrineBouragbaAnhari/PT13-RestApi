c6151e5cc6dbbec7a8d9bbd12ea9f509cad8aea3
SabrineBA <sabrinaanhari21@gmail.com>
2025-01-17 01:29:14 +0100

    Barra

--- app/src/main/java/net/iesseveroochoa/sabrinebouragba/t13_restapi/ui/screens/favoritos/FavoritosScreen.kt
+++ app/src/main/java/net/iesseveroochoa/sabrinebouragba/t13_restapi/ui/screens/favoritos/FavoritosScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import net.iesseveroochoa.sabrinebouragba.t13_restapi.R
 
 @Composable
 fun FavoritosScreen(
@@ -23,7 +24,7 @@ fun FavoritosScreen(
             .padding(40.dp),
     ) {
         Text(
-            text = "Favoritos",
+            text = R.string.label_favoritos.toString(),
             textAlign = TextAlign.Center,
             fontSize = 40.sp,
             color = MaterialTheme.colorScheme.primary,
--- app/src/main/java/net/iesseveroochoa/sabrinebouragba/t13_restapi/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/net/iesseveroochoa/sabrinebouragba/t13_restapi/ui/screens/home/HomeScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import net.iesseveroochoa.sabrinebouragba.t13_restapi.R
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
@@ -23,7 +24,7 @@ fun HomeScreen() {
             .padding(40.dp),
     ) {
         Text(
-            text = "Home",
+            text = R.string.label_home.toString(),
             textAlign = TextAlign.Center,
             fontSize = 40.sp,
             color = MaterialTheme.colorScheme.primary,
